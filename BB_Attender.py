d7c5c75f423e6ee406f95cbcfdb38aa016d23de2
mohammedaltoyan <mohammedaltoyan@gmail.com>
2021-01-26 15:34:15 +0300

    Seperation back to comma

--- BB_Attender.py
+++ BB_Attender.py
@@ -1,6 +1,6 @@
 #  Pyinstaller -F --add-binary "./driver/chromedriver.exe;./driver" BB_Attender.py
 
-Program_version = 'BB_Attender v3.02'
+Program_version = 'BB_Attender v3.03'
 print(Program_version)
 
 #todo check for access denied for blackboard if refresh rate was slow and fix it, then implement the time system it was on previous version. The problem was the program wait to the official hours and when it refreshed the access will be denied
@@ -290,7 +290,7 @@ def session_attender_1(): #select and attend the sessions
     session_numbers_input = input("\n Enter the session's number that you would like to attend separated by comma '.' NO SPACES!>>")
     refresh_rate = float(input('\n Page Refresh rate? in minutes>>'))
     print('')
-    session_numbers_list = session_numbers_input.split('.') #or names
+    session_numbers_list = session_numbers_input.split(',') #or names
     try:
         session_numbers_list = [int (i) for i in session_numbers_list ] #make the number written integers so we can call them from different dict
     except:
